784ee6de7cf4cfd8317e4c25ee03d5a6234569b6
ThyVonR <thyvonr@gmail.com>
2017-01-10 13:20:43 +0100

    Terrain generated
    Terrain is created, but still needs color, shading and water surface.

--- RobotRace/build/classes/robotrace/shaderPrograms/Terrain/vertex.glsl
+++ RobotRace/build/classes/robotrace/shaderPrograms/Terrain/vertex.glsl
@@ -3,5 +3,14 @@
 void main()
 {
     gl_Position    = gl_ModelViewProjectionMatrix * gl_Vertex;      // model view transform
-    gl_FrontColor = gl_Color;
+    if (Terrain.calcH(Terrain.i,Terrain.j) >= 0.5) {
+        gl_Color = vec4(1,0,0,0);
+    }
+    else if (Terrain.calcH(Terrain.i,Terrain.j) <= 0) {
+        gl_Color = vec4(0,0,1,0);
+    }
+    else if (Terrain.calcH(Terrain.i,Terrain.j) > 0 && Terrain.calcH(Terrain.i,Terrain.j) < 0.5) {
+        gl_Color = vec4(1,1,0,0);
+    }
+gl_FrontColor = gl_Color;
 }
