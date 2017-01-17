7724854946ebdd1c49efd99c0473f38deba3e078
ThyVonR <thyvonr@gmail.com>
2017-01-17 15:19:11 +0100

    Edited the shaders a bit
    May be closer to working

--- RobotRace/src/robotrace/shaderPrograms/Terrain/vertex.glsl
+++ RobotRace/src/robotrace/shaderPrograms/Terrain/vertex.glsl
@@ -1,7 +1,19 @@
 // simple vertex shader
 
-void main()
-{
-    gl_Position    = gl_ModelViewProjectionMatrix * gl_Vertex;      // model view transform
-    gl_FrontColor = gl_Color;
+vec4 trueColor (vec4 Position) {
+    float h = 0.3*cos(-0.4*Position.x+0.5*Position.y) + 0.7*cos(0.2*Position.x-0.7*Position.y);
+    if (h <= 0.0) {
+        return vec4(0,0,-2*h,1);
+    }
+    else if (h > 0.0 && h <= 0.5) {
+        return vec4(2*h,2*h,0,1);
+    }
+    else if (h > 0.5) {
+        return vec4(-2*h+2,1,0,1);
+    }
+}
+
+void main() {
+    gl_Position    = gl_ModelViewProjectionMatrix * gl_Vertex;
+    gl_FrontColor = trueColor(gl_Position);
 }
